113f51be3772288372452b05af6e9dfe72a37cf9
ken-dev <nhatong2015@gmail.com>
2020-03-10 14:35:45 +0700

    Express JS -Validation

--- query.js
+++ query.js
@@ -1,11 +1,44 @@
 var express = require('express')
-var app =express()
+var bodyParser = require('body-parser')
 var port = 3000;
 
+var userRoute = require('./routes/user.route');
+
+var app = express()
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+//Static file
+app.use(express.static('public'))
+//routes
 app.get('/', function(req,res){
-	res.send('<h1>Hello Word !</h1>');
-})
+	res.render('index.pug',{
+		name:'Ken'
+	});
+});
+
+app.use('/users', userRoute);
 
 app.listen(port, function(){
 	console.log('Sever listening on port :'+ port);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
